b9034b7c9ef419054dbbb80d9287058fa6def7a2
Dalton Walker <dev@daltonwalker.com>
2024-03-06 14:51:32 -0500

    adds test for add_labeling_function

--- tests/test_string_filter_functional.py
+++ tests/test_string_filter_functional.py
@@ -5,6 +5,11 @@ import pytest
 from zipfile import ZipFile
 from loguru import logger
 from at_nlp.filters.string_filter import StringFilter
+from snorkel.labeling import labeling_function
+from enum import Enum, unique
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.svm import SVC
 
 compressed_test_data_path = Path("./tests/test_data.zip")
 assert compressed_test_data_path.exists(), "Cannot find test data!"
@@ -67,6 +72,55 @@ def pre_fn_ex2(ds: pd.Series, position: int) -> pd.Series:
     ds.iat[position] = s.capitalize()
     return ds
 
+@unique
+class FilterResult(Enum):
+    """Enumeration of categories for each message"""
+
+    ABSTAIN = -1
+    ACTION = 0
+    REVIEW = 1
+    RECYCLE = 2
+
+
+# Create some labeling functions
+rsrcs = dict(col_name="col_name")
+
+
+@labeling_function(name="test_weak_learner_01", resources=rsrcs)
+def lf_fn_ex_01(series: pd.Series, col_name: str) -> int:
+    s = series[col_name]
+    if len(s) > 6:
+        return FilterResult.ABSTAIN.value
+    return FilterResult.RECYCLE.value
+
+
+@labeling_function(name="test_weak_learner_02", resources=rsrcs)
+def lf_fn_ex_02(series: pd.Series, col_name: str) -> int:
+    s: str = series[col_name]
+    if s.find("3") != -1:
+        return FilterResult.ABSTAIN.value
+    return FilterResult.RECYCLE.value
+
+
+# Create some primitive fns
+def pr_fn_ex_01(s: str) -> int:
+    if s.lower() == s:
+        return FilterResult.ABSTAIN.value
+    return FilterResult.RECYCLE.value
+
+
+def pr_fn_ex_02(s: str) -> int:
+    if s.upper() == s:
+        return FilterResult.RECYCLE.value
+    return FilterResult.ABSTAIN.value
+
+
+# Create some sklearn estimators
+rf = RandomForestClassifier()
+sv = SVC()
+mlp = MLPClassifier()
+
+
 @pytest.fixture
 def empty_filter():
     yield StringFilter()
@@ -77,6 +131,14 @@ def full_pre_filter():
     pre_filter.add_preprocessor(pre_fn_ex0)
     pre_filter.add_preprocessor(pre_fn_ex1)
     pre_filter.add_preprocessor(pre_fn_ex2)
+    yield pre_filter
+
+@pytest.fixture
+def full_lf_filter():
+    pre_filter = StringFilter()
+    pre_filter.add_labeling_function(lf_fn_ex_01)
+    pre_filter.add_labeling_function(pr_fn_ex_01)
+    pre_filter.add_labeling_function(rf)
     yield pre_filter
 
 
@@ -157,9 +219,59 @@ class TestAddPreprocessor:
         assert all([callable(fn) for fn in full_pre_filter.preprocessors])
 
 
+class TestAddLabelingFunctions:
 
+    def test_add_empty_labeling_fn(self, empty_filter):
+        empty_filter.add_labeling_function(lf_fn_ex_01)
+        assert len(empty_filter.labeling_fns) == 1
+        assert empty_filter.labeling_fns[0].fn.name == "test_weak_learner_01"
+        assert callable(empty_filter.labeling_fns[0].fn)
 
+    def test_add_empty_primitive_fn(self, empty_filter):
+        empty_filter.add_labeling_function(pr_fn_ex_01)
+        assert len(empty_filter.labeling_fns) == 1
+        assert empty_filter.labeling_fns[0].fn.__name__ == "pr_fn_ex_01"
+        assert callable(empty_filter.labeling_fns[0].fn)
 
+    def test_add_empty_sklearn_estimator(self, empty_filter):
+        empty_filter.add_labeling_function(rf)
+        assert len(empty_filter.labeling_fns) == 1
+        assert empty_filter.labeling_fns[0].fn.__class__ == RandomForestClassifier
+        assert callable(empty_filter.labeling_fns[0].fn)
+
+    def test_add_full_labeling_fn(self, full_lf_filter):
+        full_lf_filter.add_labeling_function(lf_fn_ex_01)
+        assert len(full_lf_filter.labeling_fns) == 4
+        assert full_lf_filter.labeling_fns[-1].fn.name == "test_weak_learner_01"
+        assert callable(full_lf_filter.labeling_fns[-1].fn)
+
+    def test_add_full_primitive_fn(self, full_lf_filter):
+        full_lf_filter.add_labeling_function(pr_fn_ex_01)
+        assert len(full_lf_filter.labeling_fns) == 4
+        assert full_lf_filter.labeling_fns[-1].fn.__name__ == "PR_pr_fn_ex_01"
+        assert callable(full_lf_filter.labeling_fns[-1].fn)
+
+    def test_add_full_sklearn_estimator(self, full_lf_filter):
+        full_lf_filter.add_labeling_function(rf)
+        assert len(full_lf_filter.labeling_fns) == 4
+        assert full_lf_filter.labeling_fns[-1].fn.__class__ == RandomForestClassifier
+        assert callable(full_lf_filter.labeling_fns[-1].fn)
+
+    def test_add_empty_multiple(self, empty_filter):
+        empty_filter.add_labeling_function([lf_fn_ex_01, pr_fn_ex_01, rf])
+        assert len(empty_filter.labeling_fns) == 3
+        assert empty_filter.labeling_fns[0].fn.name == "test_weak_learner_01"
+        assert empty_filter.labeling_fns[1].fn.name == "PR_pr_fn_ex_01"
+        assert empty_filter.labeling_fns[2].fn.name == "SK_RandomForestClassifier"
+        assert all([callable(wl.fn) for wl in empty_filter.labeling_fns])
+
+    def test_add_full_multiple(self, full_lf_filter):
+        full_lf_filter.add_labeling_function([lf_fn_ex_01, pr_fn_ex_01, rf])
+        assert len(full_lf_filter.labeling_fns) == 6
+        assert full_lf_filter.labeling_fns[3].fn.name == "test_weak_learner_01"
+        assert full_lf_filter.labeling_fns[4].fn.name == "PR_pr_fn_ex_01"
+        assert full_lf_filter.labeling_fns[5].fn.name == "SK_RandomForestClassifier"
+        assert all([callable(wl.fn) for wl in full_lf_filter.labeling_fns])
 
 class TestTrainTestSplit:
 
