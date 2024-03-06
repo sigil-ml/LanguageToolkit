e118905fe3b4a70050cd62923cf710c8b6f726c7
Dalton Walker <dev@daltonwalker.com>
2024-03-06 00:33:56 -0500

    automated test init

--- tests/test_string_filter.py
+++ tests/test_string_filter.py
@@ -1,3 +1,165 @@
+r"""
+   ______      _             _____ ____            ______        __
+  / __/ /_____(_)__  ___ _  / __(_) / /____ ____  /_  __/__ ___ / /_
+ _\ \/ __/ __/ / _ \/ _ `/ / _// / / __/ -_) __/   / / / -_|_-</ __/
+/___/\__/_/ /_/_//_/\_, / /_/ /_/_/\__/\__/_/     /_/  \__/___/\__/
+                   /___/
+
+
+This file tests the various functions of the StringFilter. Specific tests for the
+WeakLearners collection are in test_weak_learner_collection.py, and tests for the
+PreprocessorStack are in test_preprocessor_stack.py.
+
+We will test the following combination of functions:
+
+D := 1 if using template miner, 0 otherwise
+
+Preprocessors:
+P1 := AcronymExpansion
+P2 := Strip Non-ASCII
+P3 := Link remover
+
+Weak Learners:
+W1 := Profanity Learner                 # Labeling function Test
+W2 := sklearn RandomForestClassifier    # sklearn test
+W3 := Length Learner                    # Primitive test
+
+
+There are 2^4 * (2^3-1) = 112 possible combinations of tests. Testing the pre-processors
+individually is un-necessary since they are already tested in test_preprocessor_stack.py.
+Therefore, there are 2^4 possible configurations of pre-weak learner tests. We do not
+consider the case when all the weak learners are turned off, so we subtract one.
+
+We are interested in testing the interaction between different types of pre-processors
+and the weak learners. There are three archetypal pre-processors: csv mappings (P1),
+symbol-based (P2), and regex-based (P3). We will also test each of the accepted weak
+learner types: Snorkel labeling function (W1), Sci-kit Learn (W2), and primitives (W3)
+to ensure that they are functioning correctly.
+
+
++========================================================+
+|                      TEST CHART                        |
++=========+================+==============+==============+
+| TEST ID | Template Miner | Preprocessors| Weak Learners|
++---------+----------------+--------------+--------------+
+|         |       D        | P1 | P2 | P3 | W1 | W2 | W3 |
++---------+----------------+----+----+----+----+----+----+
+| 1       |       0        |  0 |  0 |  0 |  1 |  0 |  0 |
+| 2       |       0        |  0 |  0 |  0 |  1 |  1 |  0 |
+| 3       |       0        |  0 |  0 |  0 |  1 |  0 |  1 |
+| 4       |       0        |  0 |  0 |  0 |  0 |  1 |  1 |
+| 5       |       0        |  0 |  0 |  0 |  1 |  1 |  1 |
+| 6       |       0        |  0 |  0 |  0 |  0 |  0 |  1 |
+| 7       |       0        |  0 |  0 |  0 |  0 |  1 |  0 |
+| 9       |       0        |  1 |  0 |  0 |  1 |  0 |  0 |
+| 9       |       0        |  1 |  0 |  0 |  1 |  1 |  0 |
+| 10      |       0        |  1 |  0 |  0 |  1 |  0 |  1 |
+| 11      |       0        |  1 |  0 |  0 |  0 |  1 |  1 |
+| 12      |       0        |  1 |  0 |  0 |  1 |  1 |  1 |
+| 13      |       0        |  1 |  0 |  0 |  0 |  0 |  1 |
+| 14      |       0        |  1 |  0 |  0 |  0 |  1 |  0 |
+| 15      |       0        |  0 |  1 |  0 |  1 |  0 |  0 |
+| 16      |       0        |  0 |  1 |  0 |  1 |  1 |  0 |
+| 17      |       0        |  0 |  1 |  0 |  1 |  0 |  1 |
+| 18      |       0        |  0 |  1 |  0 |  0 |  1 |  1 |
+| 19      |       0        |  0 |  1 |  0 |  1 |  1 |  1 |
+| 20      |       0        |  0 |  1 |  0 |  0 |  0 |  1 |
+| 21      |       0        |  0 |  1 |  0 |  0 |  1 |  0 |
+| 22      |       0        |  0 |  0 |  1 |  1 |  0 |  0 |
+| 23      |       0        |  0 |  0 |  1 |  1 |  1 |  0 |
+| 24      |       0        |  0 |  0 |  1 |  1 |  0 |  1 |
+| 25      |       0        |  0 |  0 |  1 |  0 |  1 |  1 |
+| 26      |       0        |  0 |  0 |  1 |  1 |  1 |  1 |
+| 27      |       0        |  0 |  0 |  1 |  0 |  0 |  1 |
+| 28      |       0        |  0 |  0 |  1 |  0 |  1 |  0 |
+| 29      |       0        |  1 |  1 |  0 |  1 |  0 |  0 |
+| 30      |       0        |  1 |  1 |  0 |  1 |  1 |  0 |
+| 31      |       0        |  1 |  1 |  0 |  1 |  0 |  1 |
+| 32      |       0        |  1 |  1 |  0 |  0 |  1 |  1 |
+| 33      |       0        |  1 |  1 |  0 |  1 |  1 |  1 |
+| 34      |       0        |  1 |  1 |  0 |  0 |  0 |  1 |
+| 35      |       0        |  1 |  1 |  0 |  0 |  1 |  0 |
+| 36      |       0        |  1 |  0 |  1 |  1 |  0 |  0 |
+| 37      |       0        |  1 |  0 |  1 |  1 |  1 |  0 |
+| 38      |       0        |  1 |  0 |  1 |  1 |  0 |  1 |
+| 39      |       0        |  1 |  0 |  1 |  0 |  1 |  1 |
+| 40      |       0        |  1 |  0 |  1 |  1 |  1 |  1 |
+| 41      |       0        |  1 |  0 |  1 |  0 |  0 |  1 |
+| 42      |       0        |  1 |  0 |  1 |  0 |  1 |  0 |
+| 43      |       0        |  0 |  1 |  1 |  1 |  0 |  0 |
+| 44      |       0        |  0 |  1 |  1 |  1 |  1 |  0 |
+| 45      |       0        |  0 |  1 |  1 |  1 |  0 |  1 |
+| 46      |       0        |  0 |  1 |  1 |  0 |  1 |  1 |
+| 47      |       0        |  0 |  1 |  1 |  1 |  1 |  1 |
+| 48      |       0        |  0 |  1 |  1 |  0 |  0 |  1 |
+| 49      |       0        |  0 |  1 |  1 |  0 |  1 |  0 |
+| 50      |       0        |  1 |  1 |  1 |  1 |  0 |  0 |
+| 51      |       0        |  1 |  1 |  1 |  1 |  1 |  0 |
+| 52      |       0        |  1 |  1 |  1 |  1 |  0 |  1 |
+| 53      |       0        |  1 |  1 |  1 |  0 |  1 |  1 |
+| 54      |       0        |  1 |  1 |  1 |  1 |  1 |  1 |
+| 55      |       0        |  1 |  1 |  1 |  0 |  0 |  1 |
+| 56      |       0        |  1 |  1 |  1 |  0 |  1 |  0 |
+| 57      |       1        |  0 |  0 |  0 |  1 |  0 |  0 |
+| 58      |       1        |  0 |  0 |  0 |  1 |  1 |  0 |
+| 59      |       1        |  0 |  0 |  0 |  1 |  0 |  1 |
+| 60      |       1        |  0 |  0 |  0 |  0 |  1 |  1 |
+| 61      |       1        |  0 |  0 |  0 |  1 |  1 |  1 |
+| 62      |       1        |  0 |  0 |  0 |  0 |  0 |  1 |
+| 63      |       1        |  0 |  0 |  0 |  0 |  1 |  0 |
+| 64      |       1        |  1 |  0 |  0 |  1 |  0 |  0 |
+| 65      |       1        |  1 |  0 |  0 |  1 |  1 |  0 |
+| 66      |       1        |  1 |  0 |  0 |  1 |  0 |  1 |
+| 67      |       1        |  1 |  0 |  0 |  0 |  1 |  1 |
+| 68      |       1        |  1 |  0 |  0 |  1 |  1 |  1 |
+| 69      |       1        |  1 |  0 |  0 |  0 |  0 |  1 |
+| 70      |       1        |  1 |  0 |  0 |  0 |  1 |  0 |
+| 71      |       1        |  0 |  1 |  0 |  1 |  0 |  0 |
+| 72      |       1        |  0 |  1 |  0 |  1 |  1 |  0 |
+| 73      |       1        |  0 |  1 |  0 |  1 |  0 |  1 |
+| 74      |       1        |  0 |  1 |  0 |  0 |  1 |  1 |
+| 75      |       1        |  0 |  1 |  0 |  1 |  1 |  1 |
+| 76      |       1        |  0 |  1 |  0 |  0 |  0 |  1 |
+| 77      |       1        |  0 |  1 |  0 |  0 |  1 |  0 |
+| 78      |       1        |  0 |  0 |  1 |  1 |  0 |  0 |
+| 79      |       1        |  0 |  0 |  1 |  1 |  1 |  0 |
+| 80      |       1        |  0 |  0 |  1 |  1 |  0 |  1 |
+| 81      |       1        |  0 |  0 |  1 |  0 |  1 |  1 |
+| 82      |       1        |  0 |  0 |  1 |  1 |  1 |  1 |
+| 83      |       1        |  0 |  0 |  1 |  0 |  0 |  1 |
+| 84      |       1        |  0 |  0 |  1 |  0 |  1 |  0 |
+| 85      |       1        |  1 |  1 |  0 |  1 |  0 |  0 |
+| 86      |       1        |  1 |  1 |  0 |  1 |  1 |  0 |
+| 87      |       1        |  1 |  1 |  0 |  1 |  0 |  1 |
+| 88      |       1        |  1 |  1 |  0 |  0 |  1 |  1 |
+| 89      |       1        |  1 |  1 |  0 |  1 |  1 |  1 |
+| 90      |       1        |  1 |  1 |  0 |  0 |  0 |  1 |
+| 91      |       1        |  1 |  1 |  0 |  0 |  1 |  0 |
+| 92      |       1        |  1 |  0 |  1 |  1 |  0 |  0 |
+| 93      |       1        |  1 |  0 |  1 |  1 |  1 |  0 |
+| 94      |       1        |  1 |  0 |  1 |  1 |  0 |  1 |
+| 95      |       1        |  1 |  0 |  1 |  0 |  1 |  1 |
+| 96      |       1        |  1 |  0 |  1 |  1 |  1 |  1 |
+| 97      |       1        |  1 |  0 |  1 |  0 |  0 |  1 |
+| 98      |       1        |  1 |  0 |  1 |  0 |  1 |  0 |
+| 99      |       1        |  0 |  1 |  1 |  1 |  0 |  0 |
+| 100     |       1        |  0 |  1 |  1 |  1 |  1 |  0 |
+| 101     |       1        |  0 |  1 |  1 |  1 |  0 |  1 |
+| 102     |       1        |  0 |  1 |  1 |  0 |  1 |  1 |
+| 103     |       1        |  0 |  1 |  1 |  1 |  1 |  1 |
+| 104     |       1        |  0 |  1 |  1 |  0 |  0 |  1 |
+| 105     |       1        |  0 |  1 |  1 |  0 |  1 |  0 |
+| 106     |       1        |  1 |  1 |  1 |  1 |  0 |  0 |
+| 107     |       1        |  1 |  1 |  1 |  1 |  1 |  0 |
+| 108     |       1        |  1 |  1 |  1 |  1 |  0 |  1 |
+| 109     |       1        |  1 |  1 |  1 |  0 |  1 |  1 |
+| 110     |       1        |  1 |  1 |  1 |  1 |  1 |  1 |
+| 111     |       1        |  1 |  1 |  1 |  0 |  0 |  1 |
+| 112     |       1        |  1 |  1 |  1 |  0 |  1 |  0 |
++=========+================+====+====+====+====+====+====+
+
+"""
+
 from pathlib import Path
 import zipfile
 import pytest
@@ -47,11 +209,22 @@ except Exception as e:
 
 class TestTrainStringFilter:
 
-    # self.training_data = pd
+    @pytest.fixture
+    def new_filter_wo_template_miner(self):
+        str_filter = StringFilter(
+            training_col_name="text",
+            label_col_name="label",
+            use_template_miner=False
+        )
+        yield str_filter
 
     @pytest.fixture
-    def new_filter(self):
-        str_filter = StringFilter()
+    def new_filter_template_miner(self):
+        str_filter = StringFilter(
+            training_col_name="text",
+            label_col_name="label",
+            use_template_miner=True
+        )
         yield str_filter
 
     def test_training_data_exists(self):
@@ -63,6 +236,19 @@ class TestTrainStringFilter:
         logger.info(f"Num cols: {len(test_data.columns)}")
         logger.info(f"Columns: {test_data.columns}")
         logger.info("====================================")
+
+    def test_training_weak_learners_no_templates(self, new_filter_wo_template_miner):
+        train, test = new_filter_wo_template_miner.train_test_split(test_data)
+        training_results = new_filter_wo_template_miner.fit(train)
+        test_results = new_filter_wo_template_miner.eval(test)
+        assert test_results.acc > 0.9, f"Training accuracy: {test_results.acc}"
+
+    def test_training_weak_learners_no_templates(self, new_filter_template_miner):
+        train, test = new_filter_template_miner.train_test_split(test_data)
+        training_results = new_filter_template_miner.fit(train)
+        test_results = new_filter_template_miner.eval(test)
+        assert test_results.acc > 0.9, f"Training accuracy: {test_results.acc}"
+
 
     # def test_train_string_filter(self, new_filter):
     #     new_filter.fit()
